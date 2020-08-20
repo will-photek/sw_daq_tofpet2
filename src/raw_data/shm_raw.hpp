407f97f0d53a96f0a943ae004b59b09f9fb04fd2
Will Moore <will.moore@photek.co.uk>
2020-08-20 09:41:11 +0100

    Compiles

--- src/raw_data/shm_raw.hpp
+++ src/raw_data/shm_raw.hpp
@@ -6,6 +6,7 @@
 #include <event_decode.hpp>
 #include <sys/mman.h>
 #include <boost/python/numpy.hpp>
+using namespace boost::python;
 
 namespace PETSYS {
 	
@@ -69,6 +70,22 @@ struct RawDataFrame {
 	
 };
 
+typedef struct PackedEvent {
+    uint16_t asic_id;
+    uint16_t chan_id;
+    uint16_t tac_id;
+    uint16_t tcoarse;
+    uint16_t tfine;
+    uint16_t ecoarse;
+    uint16_t efine;
+
+    bool operator== (const PackedEvent& rhs) {
+        return this->asic_id == rhs.asic_id;
+    }
+} PackedEvent_t;
+
+typedef std::vector<PackedEvent_t> PackedEventVec;
+
 class SHM_RAW {
 public:
 	SHM_RAW(std::string path);
@@ -130,6 +147,38 @@ public:
 	unsigned getChannelID(int index, int event) {
 		return  getRawDataFrame(index)->getChannelID(event);
 	};
+	
+	std::shared_ptr<PackedEventVec>  getRawFrame(int index) {
+	    int nevents = getNEvents(index);
+	    int evCount = 0;
+	    RawDataFrame *dataFrame = &shm[index];
+	    std::shared_ptr<PackedEventVec> procFrame(new PackedEventVec());
+
+	    for (int i = 0; i < nevents; ++i) {
+	        //if (dataFrame->feType[i+2] == 0) {
+            uint64_t eventWord = dataFrame->data[i+2];
+            PackedEvent_t p;
+
+            p.tcoarse = (eventWord >> 38) & 0x3FF;
+            p.ecoarse = (eventWord >> 18) & 0x3FF;
+            p.tfine = (eventWord >> 28) & 0x3FF;
+            p.efine = (eventWord >> 8) & 0x3FF;
+
+            p.tac_id = eventWord & 0x3;
+            p.chan_id = (eventWord >> 2) & 0x3F;
+
+            uint64_t idWord = eventWord >> 48;
+            uint64_t asicID = idWord & 0x3F;
+            uint64_t slaveID = (idWord >> 6) & 0x1F;
+            uint64_t portID = (idWord >> 11) & 0x1F;
+            p.asic_id = (slaveID & 0x1) * 16*16 + (portID & 0xF) * 16 + (asicID & 0xF);
+
+            procFrame->push_back(p);
+	        //}
+	    }
+
+	    return procFrame;
+	}
 
 private:
 	int shmfd;
--- src/raw_data/shm_raw_py.cpp
+++ src/raw_data/shm_raw_py.cpp
@@ -5,6 +5,44 @@ using namespace boost::python;
 #include "shm_raw.hpp"
 using namespace PETSYS;
 
+inline void destroyArrayOwnerPtr(_object *p) {
+   std::shared_ptr<PackedEventVec> *b = reinterpret_cast<std::shared_ptr<PackedEventVec>*>(p);
+   delete b;
+}
+
+inline boost::python::object makeArrayOwner(std::shared_ptr<PackedEventVec> & x) {
+    boost::python::handle<> h(PyCapsule_New(new std::shared_ptr<PackedEventVec>(x), NULL,
+        &destroyArrayOwnerPtr));
+    return boost::python::object(h);
+}
+
+numpy::ndarray frame2numpy(SHM_RAW& obj, int index) {
+	// Get one frame of data, the PackedEventVec contains a continuous
+	// region of structs with even packing. A shared_ptr must be used
+	// here as otherwise C++ would free the Vector's memory once this
+	// func returns.
+	std::shared_ptr<PackedEventVec> frame = obj.getRawFrame(index);
+
+	// Setup up numpy array parameters
+	tuple shape = make_tuple(frame->size(), 7);
+	tuple stride = make_tuple(sizeof(RawDataFrame), sizeof(uint16_t));
+	numpy::dtype dt = numpy::dtype(str("u2"), false);
+
+	// The created numpy array must have a way to indicate to C++ that
+	// the created python object has been destroyed. To do this an empty
+	// PyObject (or python::object to boost) is created, that when the
+	// the numpy array's ref count in python reaches zero it calls a
+	// destruction function (destroyArrayOwnerPtr), that deletes it's
+	// reference to the shared_ptr, then C++ can decrement its reference
+	// counter and free memory if required.
+	object own = makeArrayOwner(frame);
+
+	// Finally build the numpy array
+	// frame is a uint64_t with methods now - this will need to be done differently.
+	numpy::ndarray arr = numpy::from_data(frame->data(), dt, shape, stride, own);
+	return arr;
+}
+
 BOOST_PYTHON_MODULE(shm_raw)
 {
 	numpy::initialize();
